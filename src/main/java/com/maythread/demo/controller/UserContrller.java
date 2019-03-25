2608beb722d65d40590be7a3544594be77824d09
chenglong <3469474636@qq.com>
2019-03-25 19:27:23 +0800

    线程创建的两种方式

--- src/main/java/com/maythread/demo/controller/UserContrller.java
+++ src/main/java/com/maythread/demo/controller/UserContrller.java
@@ -3,11 +3,14 @@ package com.maythread.demo.controller;
 import com.maythread.demo.entity.UserEntity;
 import com.maythread.demo.service.UserService;
 import org.apache.ibatis.annotations.Param;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 
 @RestController
@@ -19,5 +22,27 @@ public class UserContrller {
     public UserEntity userMapper(HttpServletRequest request, UserEntity userEntity){
         userEntity=userService.selectById(userEntity.getId());
         return userEntity;
+    }
+
+    @Test
+    public void CreateThread(){
+        /**第一种线程方式*/
+       /* new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("线程开始了");
+            }
+        }).start();*/
+       /**第二种线程方式*/
+        Callable<Boolean>userCall=new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                System.out.println("线程开始了！！！");
+                return true;
+            }
+        };
+        FutureTask<Boolean>userFutureTask=new FutureTask<Boolean>(userCall);
+        new Thread(userFutureTask).start();
+
     }
 }
